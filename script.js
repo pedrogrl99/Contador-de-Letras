0757efc8cd8d0c25a91815a8337e89342f9dcafc
Pedro Guilherme <93205094+pedrogrl99@users.noreply.github.com>
2024-10-01 23:15:36 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const textArea = document.getElementById('textArea');
-const charCount = document.getElementById('charCount');
-
-function ler (){
-    const textLength = textArea.value.length;
-    charCount.textContent = `Caracteres: ${textLength}`;
-};
